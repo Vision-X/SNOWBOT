40418b485801698e715fac2fcaa41a48edc76d91
Vision-X <michael.s.marlow@gmail.com>
2018-11-03 10:43:22 -0600

    light refactor, beautified, removed functional promises lib for now

--- commands/snow.js
+++ commands/snow.js
@@ -1,67 +1,53 @@
 module.exports = {
-  name: 'snow',
-  description: 'snow totals',
+  name: "snow",
+  description: "snow totals",
   args: true,
   execute(message, args, data) {
-    // const fs = require('fs');
-    // const FP = require('functional-promise');
     let WEATHERDATA;
-    // console.log("ARGS... ", args);
 
     if (!args.length) {
       return message.channel.send(
-          `You didn't provide any arguments, ${message.author}!
+        `You didn't provide any arguments, ${message.author}!
           \n Type ** !snow all ** to see all snow reports
           \n Type ** !snow keystone or keyst or key ** to see keystone (in this example)
           \n Type ** !snow resorts ** or ** !resorts ** to see the list of resort commands
-          \n`)
-    } else if (args[0] == 'all') {
-        console.log("args is all :", args[0]);
-        let WEATHERDATA = data;
-        let fullMessage = "";
-        // let text = "``` " + `${resort.resortName.toUpperCase()} Snow Report
-        // \n Snow  [ 24hr: ${resort.lastDayTotal}" 72hr: ${resort.threeDayTotal}" ]
-        // \n Depth [ Base: ${resort.baseDepth}" Top: ${resort.topDepth}" ]
-        // \n Lifts [ ${resort.liftNum} open out of ${resort.liftTotal} ]` + "```";
-        let num = 1;
-        WEATHERDATA.map((resort) => {
-          let text = `[${num}] - ${resort.resortName.toUpperCase()} \n`;
-          fullMessage += text
-          num++;
-        })
-        console.log("Full message: ", fullMessage);
-        message.channel.send("```" + fullMessage + "```")
-
+          \n`
+      );
+    } else if (args[0].toLowerCase() == "all") {
+      console.log("args is all :", args[0]);
+      let WEATHERDATA = data;
+      let fullMessage = "";
+      let num = 1;
+      WEATHERDATA.map(resort => {
+        let text = `[${num}] - ${resort.resortName.toUpperCase()} \n`;
+        fullMessage += text;
+        num++;
+      });
+      console.log("Full message: ", fullMessage);
+      message.channel.send("```" + fullMessage + "```");
     } else {
-        let WEATHERDATA = data;
-        const sendMess = () => {
-          if (typeof WEATHERDATA != 'undefined') {
-              let text = "``` " + `${WEATHERDATA.resortName.toUpperCase()} Snow Report
-              \n Snow  [ 24hr: ${WEATHERDATA.lastDayTotal}" 72hr: ${WEATHERDATA.threeDayTotal}" ]
-              \n Depth [ Base: ${WEATHERDATA.baseDepth}" Top: ${WEATHERDATA.topDepth}" ]
-              \n Lifts [ ${WEATHERDATA.liftNum} open out of ${WEATHERDATA.liftTotal} ]` + "```";
-              message.channel.send(text)
-
-
-          // else if (args[0] == 'all') {
-          //   console.log("args is all :", args[0]);
-          //   let fullMessage = "";
-          //   // let text = "``` " + `${resort.resortName.toUpperCase()} Snow Report
-          //   // \n Snow  [ 24hr: ${resort.lastDayTotal}" 72hr: ${resort.threeDayTotal}" ]
-          //   // \n Depth [ Base: ${resort.baseDepth}" Top: ${resort.topDepth}" ]
-          //   // \n Lifts [ ${resort.liftNum} open out of ${resort.liftTotal} ]` + "```";
-          //   let text = "```" + `${resort.resortName.toUpperCase()}` + "```";
-          //   WEATHERDATA.map((resort) => {
-          //     // message.channel.send(text);
-          //     fullMessage += text
-          //   })
-          //   console.log("Full message: ", fullMessage);
-          //   message.channel.send(fullMessage)
-          } else {
-            let text = "*** resort not found ***"
-            message.channel.send(text);
-          }
+      let WEATHERDATA = data;
+      const sendMess = () => {
+        if (typeof WEATHERDATA != "undefined") {
+          let text =
+            "``` " +
+            `${WEATHERDATA.resortName.toUpperCase()} Snow Report
+              \n Snow  [ 24hr: ${WEATHERDATA.lastDayTotal}" 72hr: ${
+              WEATHERDATA.threeDayTotal
+            }" ]
+              \n Depth [ Base: ${WEATHERDATA.baseDepth}" Top: ${
+              WEATHERDATA.topDepth
+            }" ]
+              \n Lifts [ ${WEATHERDATA.liftNum} open out of ${
+              WEATHERDATA.liftTotal
+            } ]` +
+            "```";
+          message.channel.send(text);
+        } else {
+          let text = "*** resort not found ***";
+          message.channel.send(text);
         }
+      };
       sendMess();
     }
   }
--- index.js
+++ index.js
@@ -16,8 +16,8 @@ bot.commands = new Discord.Collection();
 const commandLib = fs.readdirSync('./commands').filter(f => f.endsWith('.js'));
 
 for (const file of commandLib) {
-	const command = require(`./commands/${file}`);
-	bot.commands.set(command.name, command);
+  const command = require(`./commands/${file}`);
+  bot.commands.set(command.name, command);
 };
 
 const express = require("express");
@@ -27,117 +27,109 @@ const cors = require("cors");
 
 var WEATHERDATA;
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(bodyParser.json());
 app.use(cors());
 
 
 bot.on('message', function(message) {
-	const prefix = '!';
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const command = args.shift().toLowerCase();
+  const prefix = '!';
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
 
-	if (!bot.commands.has(command)) return;
+  if (!bot.commands.has(command)) return;
 
-	var superObj = [];
-	var selection;
+  var superObj = [];
+  var selection;
 
-	if (command == "snow") {
-		selection = args[0];
-		// fetchSnowData();
-	};
+  if (command == "snow") {
+    selection = args[0].toLowerCase();
+    // fetchSnowData();
+  };
 
-	function fetchSnowData() {
-		console.log("fetching snow data...");
-		let snowUrl = 'https://skiapp.onthesnow.com/app/widgets/resortlist?region=us&regionids=251&language=en&pagetype=skireport&direction=+1'
-		fetch(snowUrl)
-		.then(res => res.json())
-		.then(sortWeather)
-		.catch()
-	};
+  function fetchSnowData() {
+    console.log("fetching snow data...");
+    let snowUrl = 'https://skiapp.onthesnow.com/app/widgets/resortlist?region=us&regionids=251&language=en&pagetype=skireport&direction=+1'
+    fetch(snowUrl)
+      .then(res => res.json())
+      .then(sortWeather)
+      .catch()
+  };
 
-	function sortWeather(res) {
-		let resorts = res.rows;
-	  for (var i = 0; i < resorts.length; i++) {
-			let singleResortName = resorts[i].resort_name.toLowerCase().replace(/ /g,'');
-			resObj = {};
-			resObj.resortName = singleResortName;
-	    resObj.threeDayTotal = Math.round(resorts[i].pastSnow.sum3/2.54 || 0);
-	    resObj.lastDayTotal = Math.round(resorts[i].pastSnow.snow0day/2.54);
-	    resObj.liftNum =  (resorts[i].snowcone.lifts_open === null) ? 0 :
-												 resorts[i].snowcone.lifts_open;
-	    resObj.liftTotal = resorts[i].resortProfile.num_lifts;
-	    resObj.baseDepth = Math.round(resorts[i].snowcone.base_depth_cm/2.54);
-	    resObj.topDepth = Math.round(resorts[i].snowcone.top_depth_cm/2.54);
-			superObj.push(resObj);
-	  }
-		console.log("state being saved...");
-		// saveState();
-		return superObj;
-	};
+  function sortWeather(res) {
+    let resorts = res.rows;
+    for (var i = 0; i < resorts.length; i++) {
+      let singleResortName = resorts[i].resort_name.toLowerCase().replace(/ /g, '');
+      resObj = {};
+      resObj.resortName = singleResortName;
+      resObj.threeDayTotal = Math.round(resorts[i].pastSnow.sum3 / 2.54 || 0);
+      resObj.lastDayTotal = Math.round(resorts[i].pastSnow.snow0day / 2.54);
+      resObj.liftNum = (resorts[i].snowcone.lifts_open === null) ? 0 : resorts[i].snowcone.lifts_open;
+      resObj.liftTotal = resorts[i].resortProfile.num_lifts;
+      resObj.baseDepth = Math.round(resorts[i].snowcone.base_depth_cm / 2.54);
+      resObj.topDepth = Math.round(resorts[i].snowcone.top_depth_cm / 2.54);
+      superObj.push(resObj);
+    }
+    console.log("state being saved...");
+    // saveState();
+    return superObj;
+  };
 
-	function saveState() {
-		console.log("saveState function");
-		fs.writeFile('snowData-state.json', JSON.stringify(superObj, null, 4), function(err) {
-			console.log('File successfuly written!!!');
-		});
-	}
+  function saveState() {
+    console.log("saveState function");
+    fs.writeFile('snowData-state.json', JSON.stringify(superObj, null, 4), function(err) {
+      console.log('File successfuly written!!!');
+    });
+  }
 
-	function getState() {
+  function getState() {
+    fs.readFile('snowData-state.json', 'utf8', function(err, data) {
+      if (err) throw err;
+      let myObj = JSON.parse(data);
+      return myObj;
+    })
+  }
 
-		fs.readFile('snowData-state.json', 'utf8', function(err, data) {
-			if (err) throw err;
-			let myObj = JSON.parse(data);
-			return myObj;
-			// console.log(Object.values(myObj['vail']));
-		})
-	}
-
-	if (command.args && !args.length) {
-		return message.channel.send(
-`You didn't provide any arguments,
+  if (command.args && !args.length) {
+    return message.channel.send(
+      `You didn't provide any arguments,
 ${message.author}! \n
 Type ** !snow all ** to see all snow reports \n
 Type ** !snow keystone ** to see a specific resort, keystone in this example \n
 Type ** !snow resorts ** or ** !resorts ** to see the list of resort commands \n`)
-	}
+  }
 
-	try {
+  try {
 
-		if (command === 'snow' && args[0].toLowerCase() != 'all') {
-			selection = args[0].toLowerCase();
-			const getFSData = async () => {
-				const snow = require('./snowData-state.json');
-				let resorts = snow.filter((obj) => {
-						console.log(obj.resortName);
-						return obj.resortName.includes(selection);
-				});
-				const snowData = await resorts[0];
-				// console.log("input: ", selection);
-				// console.log(snowData);
-				const mess = await bot.commands.get(command).execute(message, args, snowData)
-				return mess;
-				};
-			getFSData();
-		} else if (command === 'snow' && args[0].toLowerCase() == 'all') {
-			selection = args[0].toLowerCase();
-			const getFSData = async () => {
-				const snow = require('./snowData-state.json');
-				const snowData = await snow;
-				const mess = await bot.commands.get(command).execute(message, args, snowData);
-				return mess;
-				};
-			getFSData();
-		} else {
-			bot.commands.get(command).execute(message, args);
-		}
-	}
-
-	catch (error) {
-		console.error(error);
-		message.reply('there was an error with that command, BRO!');
-	}
+    if (command === 'snow' && args[0].toLowerCase() != 'all') {
+      selection = args[0].toLowerCase();
+      const getFSData = async () => {
+        const snow = require('./snowData-state.json');
+        const resorts = snow.filter((obj) => obj.resortName.includes(selection));
+        const snowData = await resorts[0];
+        const mess = await bot.commands.get(command).execute(message, args, snowData)
+        return mess;
+      };
+      getFSData();
+    } else if (command === 'snow' && args[0].toLowerCase() == 'all') {
+      selection = args[0].toLowerCase();
+      const getFSData = async () => {
+        const snow = require('./snowData-state.json');
+        const snowData = await snow;
+        const mess = await bot.commands.get(command).execute(message, args, snowData);
+        return mess;
+      };
+      getFSData();
+    } else {
+      bot.commands.get(command).execute(message, args);
+    }
+  } catch (error) {
+    console.error(error);
+    message.reply('there was an error with that command, BRO!');
+  }
 
 });
 
